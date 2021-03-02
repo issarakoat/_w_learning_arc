99251ba97ea8df125976add445b77fc9ddc817a4
Tom Issarakoat <tissarakoat@adminisatorsmbp.lan>
2021-03-02 16:11:59 -0700

    Using Environment Variables and Secrets

---
+++ environment/index.js
@@ -0,0 +1,5 @@
+/*  /environment/index.js  */
+
+export default {
+    OMDB_API_KEY: "%{AQICAHhJ+45n5+SNVeEbiGeVtt0g0uv3dy9ufUimkQvIH6egGQGZ2upbjWc9WTRU2kqrwV4hAAAAZjBkBgkqhkiG9w0BBwagVzBVAgEAMFAGCSqGSIb3DQEHATAeBglghkgBZQMEAS4wEQQMYq41esc4oGXDrU0lAgEQgCOTJ7EfkpiAPA+we6qJgMi5j12mr1JsWoEcDh1895hLEbayjA=}"
+  }
